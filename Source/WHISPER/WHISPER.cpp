14ecc11474f9914b2f1f1ec511fbb3fc6cb552ed
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2026-04-12 20:19:13 -0400

    renew files. Should work now

---
+++ Source/WHISPER/WHISPER.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WHISPER.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WHISPER, "WHISPER" );
+
+DEFINE_LOG_CATEGORY(LogWHISPER)
---
+++ Source/WHISPER/WHISPERGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WHISPERGameMode.h"
+
+AWHISPERGameMode::AWHISPERGameMode()
+{
+	// stub
+}
